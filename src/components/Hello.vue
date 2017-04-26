c83a25fa7344f568721883a83a430da381e721f2
limweb <limweb@hotmail.com>
2017-04-26 12:08:29 +0700

    firstcommit

---
+++ src/components/Hello.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+       <button class="btn btn-danger btn-lg"
+        @click="showTop = !showTop">
+        Click to toggle alert on top
+      </button>
+
+      <alert v-model="showTop" placement="top" duration="3000" type="danger" width="400px" dismissable>
+        <span class="icon-info-circled alert-icon-float-left"></span>
+        <strong>Heads up!</strong>
+        <p>This alert needs your attention.</p>
+      </alert>
+
+      <el-button @click="visible = true">Button</el-button>
+      <el-dialog v-model="visible" title="Hello world">
+      <p>Try Element</p>
+      </el-dialog>
+  </div>
+</template>
+
+<script>
+import { alert } from 'vue-strap'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      visible: false,
+      showTop: true
+    }
+  },
+  components: {
+    alert
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
